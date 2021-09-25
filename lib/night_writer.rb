91bb71131c607335320e4fcf9dbc6381b32387b6
Devin Pile <devinpile@gmail.com>
2021-09-24 19:54:47 -0600

    Testing cli input with filepath

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -7,12 +7,18 @@ class NightWriter
 
   # end
 
-  def read_file(file_path)
-    message = "Testing the read and write for Night Writer"
+  def get_cli_input
+    ARGV
+  end
+
+  def read_and_write_to_file(file_path)
+    message = ARGV
     File.open(file_path, 'a+') do |file|
       file.write(message)
     end
   end
 end
+
+NightWriter.new.get_cli_input
 
 NightWriter.new.read_file('write_to_file.txt')
