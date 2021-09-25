b35fc934eda6d3fdad0557e6c744b03f7dd9a88d
Devin Pile <devinpile@gmail.com>
2021-09-24 20:35:33 -0600

    Refactor- Add changes, delete files, overall cleanup for organization

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,24 +1,31 @@
 class NightWriter
 
-  # def initialize #(file_path)
-
+  def initialize()
 # read contents fo a file in ruby
 # write it to another file
-
-  # end
+  end
 
   def get_cli_input
     ARGV
   end
 
-  def read_and_write_to_file(file_path)
-    message = ARGV
-    File.open(file_path, 'a+') do |file|
-      file.write(message)
-    end
+
+  def print_text
+    file = File.open("message.txt").read
+    puts file
   end
 end
 
-NightWriter.new.get_cli_input
 
-NightWriter.new.read_file('write_to_file.txt')
+#   def read_and_write_to_file(file_path)
+#     message = ARGV
+#     File.open(file_path, 'a+') do |file|
+#       file.write(message)
+#     end
+#   end
+# end
+
+# NightWriter.new.read_file('message.txt')
+
+# NightWriter.new.print_text
+NightWriter.new.get_cli_input
--- spec/night_writer_spec.rb
+++ spec/night_writer_spec.rb
@@ -3,7 +3,7 @@ require './lib/night_writer'
 
 describe NightWriter do
   it 'exists' do
-    night_writer = NightWriter.new()
+    night_writer = NightWriter.new
 
     expect(night_writer).to be_a NightWriter
   end
