9aa69e8c74e8afe2d42015f105128227537fc3f4
Devin Pile <devinpile@gmail.com>
2021-09-24 18:55:49 -0600

    Testing command line interface

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,16 +1,18 @@
 class NightWriter
 
-  def initialize #(file_path)
+  # def initialize #(file_path)
 
 # read contents fo a file in ruby
 # write it to another file
 
-  end
+  # end
 
-  def print_message
-    file = File.open("message.txt").read
-    puts file
+  def read_file(file_path)
+    message = "Testing the read and write for Night Writer"
+    File.open(file_path, 'a+') do |file|
+      file.write(message)
+    end
   end
 end
 
-NightWriter.new.print_message
+NightWriter.new.read_file('write_to_file.txt')
