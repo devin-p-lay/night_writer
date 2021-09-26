d9fcb45518fcb0ab1e1937c38e449ec18c6150d1
Devin Pile <devinpile@gmail.com>
2021-09-25 21:26:13 -0600

    Add changes to braille library to support early process

--- lib/braille_library.rb
+++ lib/braille_library.rb
@@ -1,39 +1,46 @@
 class BrailleLibrary
+  attr_reader :letters,
+              :braille
 
-  def letters
+  def initialize(letter: nil, braille: nil)
+    @letters = [:letter]
+    @braille = [:braille]
+  end
+
+  def search(char)
+    characters[char]
+    end
+  end
+
+  def characters
     {
-      a: ['O.','..','..'],
-      b: ['O.','O.','..'],
-      c: ['OO','..','..'],
-      d: ['OO','.O','..'],
-      e: ['O.','.O','..'],
-      f: ['OO','O.','..'],
-      g: ['OO','OO','..'],
-      h: ['O.','OO','..'],
-      i: ['.O','O.','..'],
-      j: ['.O','OO','..'],
-      k: ['O.','..','O.'],
-      l: ['O.','O.','O.'],
-      m: ['OO','..','O.'],
-      n: ['OO','.O','O.'],
-      o: ['O.','.O','O.'],
-      p: ['OO','O.','O.'],
-      q: ['OO','OO','O.'],
-      r: ['O.','OO','O.'],
-      s: ['.O','O.','O.'],
-      t: ['.O','OO','O.'],
-      u: ['O.','..','OO'],
-      v: ['O.','O.','OO'],
-      w: ['.O','OO','.O'],
-      x: ['OO','..','OO'],
-      y: ['OO','.O','OO'],
-      z: ['O.','.O','OO'],
+      'a' => ['O.','..','..'],
+      'b' => ['O.','O.','..'],
+      'c' => ['OO','..','..'],
+      'd' => ['OO','.O','..'],
+      'e' => ['O.','.O','..'],
+      'f' => ['OO','O.','..'],
+      'g' => ['OO','OO','..'],
+      'h' => ['O.','OO','..'],
+      'i' => ['.O','O.','..'],
+      'j' => ['.O','OO','..'],
+      'k' => ['O.','..','O.'],
+      'l' => ['O.','O.','O.'],
+      'm' => ['OO','..','O.'],
+      'n' => ['OO','.O','O.'],
+      'o' => ['O.','.O','O.'],
+      'p' => ['OO','O.','O.'],
+      'q' => ['OO','OO','O.'],
+      'r' => ['O.','OO','O.'],
+      's' => ['.O','O.','O.'],
+      't' => ['.O','OO','O.'],
+      'u' => ['O.','..','OO'],
+      'v' => ['O.','O.','OO'],
+      'w' => ['.O','OO','.O'],
+      'x' => ['OO','..','OO'],
+      'y' => ['OO','.O','OO'],
+      'z' => ['O.','.O','OO']
 
     }
   end
-
-  def find(char)
-    letters.find do |letter|
-      letter == char
-    end
 end
