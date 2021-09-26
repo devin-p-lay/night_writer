b9460c4f5108492306aea62008303347ea09b5cf
Devin Pile <devinpile@gmail.com>
2021-09-25 21:25:33 -0600

    Test- Add tests for search method

--- spec/braille_library_spec.rb
+++ spec/braille_library_spec.rb
@@ -3,25 +3,20 @@ require './lib/braille_library'
 
 describe BrailleLibrary do
   before :each do
-    @bl = BrailleLibrary.new()
+    @bl = BrailleLibrary.new(letter: nil, braille: nil)
   end
 
   it 'exists' do
     expect(@bl).to be_a BrailleLibrary
   end
-  #
-  # it 'has braille input' do
-  #   char = {
-  #     braille: '.0\n0.\n..',
-  #     letter: 'a'
-  #   }
-  #   # position = {
-  #   #   top:    '.0',
-  #   #   middle: '0.',
-  #   #   bottom: '..'
-  #   # }
-  #   expect(bl.search(a:)).to be_a String
-  #   require "pry"; binding.pry
-  # end
 
+  describe '#search' do
+    it 'can #search for a character' do
+      expect(@bl.search('a')).to be_an Array
+      expect(@bl.search('b')).to eq(['O.','O.','..'])
+      expect(@bl.search('c')[0]).to eq('OO')
+      expect(@bl.search('d')[1]).to eq('.O')
+      expect(@bl.search('e')[2]).to eq('..')
+    end
+  end
 end
